6087aa2362f9082180524efcc615b8a1408f2d76
jebread <sangje112@gmail.com>
2024-07-11 15:09:38 +0900

    feat: 초기 구조 설정

---
+++ welcome/tailwind.config.ts
@@ -0,0 +1,31 @@
+import type {Config} from 'tailwindcss';
+
+const config: Config = {
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+		colors: {
+			blue: '#0081FF',
+			black: '#2D2D2D',
+			green: '#00B69B',
+			yellow: '#FCBE2D',
+			red: '#FD5454',
+			gray: {
+				100: '#FAFAFB',
+				200: '#F8F8FA',
+				300: '#F5F6F8',
+				900: '#636566',
+			},
+		},
+	},
+};
+export default config;
