4bce43c8eead0e538b2af4298453d8c0b507e84c
saemii-24 <hims2419@gmail.com>
2024-07-15 17:57:02 +0900

    feat: side 오픈 기능

--- welcome/src/components/SideMenu.tsx
+++ welcome/src/components/SideMenu.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+'use client';
+import React, {useState} from 'react';
 import {Icon} from './Icon';
 import {FaAngleDown} from 'react-icons/fa6';
+import {VscCircleFilled} from 'react-icons/vsc';
 
 const SideMenu = ({
 	data,
@@ -10,34 +12,66 @@ const SideMenu = ({
 	isHovered,
 	isSelected,
 }: any) => {
+	const [open, setOpen] = useState<boolean>(false);
+
 	return (
-		<div
-			onClick={onClick}
-			onMouseEnter={onMouseEnter}
-			onMouseLeave={onMouseLeave}
-			className={`mb-[10px] flex h-[44px] cursor-pointer items-center justify-between rounded-[8px] `}>
-			{/* 좌측 바 */}
+		<div className='bg-yellow-900'>
 			<div
-				className={`mr-[16px] h-[44px] w-[6px] rounded-r-[8px] ${isSelected ? 'bg-blue-900' : isHovered ? 'bg-blue-900' : ''} `}></div>
-			<div
-				className={`h-full grow rounded-[8px] ${isSelected ? 'bg-blue-900' : ''} px-[10px]`}>
+				onClick={onClick}
+				onMouseEnter={onMouseEnter}
+				onMouseLeave={onMouseLeave}
+				className={`relative flex h-[54px] w-[214px] cursor-pointer items-center justify-between rounded-[8px] `}>
+				{/* 좌측 바 */}
 				<div
-					className={`-mt-px flex size-full items-center gap-1 ${isSelected ? 'text-white' : 'text-gray-900'}`}>
-					<Icon icon={data.icon} />
+					className={`mr-[16px] h-[44px] w-[6px] rounded-r-[8px] ${isHovered ? 'bg-blue-900' : ''} `}></div>
+				<div
+					className={`h-[44px] grow rounded-[8px] ${isSelected ? 'bg-blue-900' : ''} px-[10px]`}>
 					<div
-						className={`font-bold ${isSelected ? 'text-white' : 'text-gray-900'}`}>
-						{data.title}
+						className={`-mt-px flex size-full items-center gap-1 ${isSelected ? 'text-white' : 'text-gray-900'}`}>
+						<Icon icon={data.icon} />
+						<div
+							className={`font-bold ${isSelected ? 'text-white' : 'text-gray-900'}`}>
+							{data.title}
+						</div>
 					</div>
 				</div>
+
+				{data.sub && (
+					<div
+						className={`absolute right-0 z-10 transition duration-500 ease-in-out ${isSelected ? 'rotate-180' : ''}`}>
+						<FaAngleDown
+							style={{
+								fontSize: '14px',
+								color: isSelected ? '#fff' : '#636566',
+								marginLeft: '16px',
+								marginRight: '16px',
+							}}
+						/>
+					</div>
+				)}
 			</div>
-			<FaAngleDown
-				style={{
-					fontSize: '12px',
-					color: '#636566',
-					marginLeft: '16px',
-					marginRight: '16px',
-				}}
-			/>
+			{/* 서브 */}
+			{data.sub && (
+				<div
+					className={`${isSelected ? 'max-h-[100px]' : 'invisible max-h-0'}  overflow-hidden text-center text-sm transition-all duration-500 ease-in-out`}>
+					{data['sub'].map((sub: string, i: number) => {
+						return (
+							<div
+								key={i}
+								className={`ml-[34px] flex cursor-pointer items-center gap-3 p-[3px] font-medium hover:text-blue-900`}>
+								<div>
+									<VscCircleFilled
+										style={{
+											fontSize: '8px',
+										}}
+									/>
+								</div>
+								<div className='text-[14px]'>{sub}</div>
+							</div>
+						);
+					})}
+				</div>
+			)}
 		</div>
 	);
 };
--- welcome/src/data/side.ts
+++ welcome/src/data/side.ts
@@ -1,3 +1,9 @@
+interface IsideData {
+	icon: string;
+	title: string;
+	sub?: string[];
+}
+
 export const sideData = [
 	{
 		icon: 'home',
@@ -6,6 +12,7 @@ export const sideData = [
 	{
 		icon: 'center',
 		title: '상담사 현황',
+		sub: ['상담사 관리', '상담사 목록', '상담 처리 현황'],
 	},
 	{
 		icon: 'log',
